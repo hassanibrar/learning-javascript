87588e42b0f97f7027b2a747db461ebb204c40dc
hassanibrar <47942327+hassanibrar@users.noreply.github.com>
2025-05-17 17:51:43 +0000

    learning variables basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountID = 144553
+let accountEmail = "hassan@gmai.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+let accountState
+
+
+// accountID = 234 // const keyword cannot be changed like this remember this and it is not allowedc00
+
+accountEmail = "hass@gmail.com"
+accountPassword = "49434"
+accountCity = "Lahore"
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope so use const and let 
+*/
+
