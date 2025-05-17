7f87c906d8a39d9aafc59ce41b0eef29ff95588c
hassanibrar <47942327+hassanibrar@users.noreply.github.com>
2025-05-17 18:38:45 +0000

    learning data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"Use Strict" // treat all JS code as newer version of javascript
+
+// alert (3+3) // we are using nodejs , not browser . We can do it in browser but not here and the browser method is old method
+
+console.log(3
+    +
+    3) // code readability should be high
+
+
+console.log(3+19873233310)
+console.log("Ittrat")
+
+
+// types of data types 
+let name = "hassan"  
+// this is a string data type above , and remember that name is variable
+
+let age = 18 
+// this is a number value data type
+
+let isLoggedIn = false
+// this is boolen type data type
+
+// number  = has a range of 2 to power of 53
+// bigint = if number is very big then use bigint especially trading and stock market website or very big website like reddit and facebook , but we will not use bigint as often
+// string = can be use in single or double quotes but it is recommended to use double quotes because its easy
+//boolean = it is a simple yes or no like true / false e.g that user is logged in or not , user had credit details or not , responses has been given by server or not
+//null = it is a standalone value , null also means empty
+//undefined = variable has been declared but value has not been defined means no value is given
+
+
